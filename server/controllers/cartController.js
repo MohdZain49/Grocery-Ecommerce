032692d40e60f57351d04853171af5c73efde55f
Mohd Zain <mohammedzain4099@gmail.com>
2025-04-12 21:21:03 +0530

    feat: add address management functionality with add and get endpoints

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -8,6 +8,7 @@ export const updateCart = async (req, res) => {
     await User.findByIdAndUpdate(userId, { cartItems });
     res.json({ success: true, message: "Cart Updated" });
   } catch (error) {
+    console.log(error.message);
     res.json({
       success: false,
       message: error.message,
